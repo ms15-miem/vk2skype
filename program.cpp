84801bf50bd641ded6479f941038e9cfd27c6789
narcjkeee <narc@bk.ru>
2013-06-03 17:03:15 +0400

    -proxy

--- program.cpp
+++ program.cpp
@@ -7,14 +7,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-//    a.setQuitOnLastWindowClosed(false);
     a.setApplicationName("vk2skype");
-
-    QNetworkProxy proxy;
-    proxy.setType(QNetworkProxy::Socks5Proxy);
-    proxy.setHostName("127.0.0.1");
-    proxy.setPort(9999);
-    QNetworkProxy::setApplicationProxy(proxy);
 
     Server server;
 
